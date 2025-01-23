cb4b3200e43c8cca72ef0731c3dc854c15329ba3
Yuta <yroger2003@gmail.com>
2025-01-23 12:54:37 +0100

    initialisation du dépot

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'file-rouge',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/file-rouge',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
